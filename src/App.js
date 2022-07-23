f971a7312cffdca0e951f7d5765c5e6bf845e70b
Mohamad Tayeb <m.tayeb993@gmail.com>
2022-07-23 15:25:53 +0300

    complete homepage

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/products' element={<Products />} />
+        <Route exact path='/products/:id' element={<SingleProduct />} />
+        <Route exact path='/checkout' element={<Checkout />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            ab animi minus architecto esse? Error, quia illo natus esse ducimus
+            culpa harum aspernatur recusandae rerum iure quis rem, vero nisi
+            nulla, in fugiat voluptatum dicta autem nam perferendis corrupti
+            quidem. Recusandae explicabo officia delectus. Sint tempore incidunt
+            quia quos voluptate?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
